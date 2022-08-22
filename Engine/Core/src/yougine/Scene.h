95c3457bf0723f577fc57c0d1a79d0f2ea5a5bd3
uni101 <Hanacchiglon07@gmail.com>
2022-08-22 15:55:35 +0900

    add scene

---
+++ Engine/Core/src/yougine/Scene.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "GameObject.h"
+#include <list>
+
+namespace yougine
+{
+	class Scene
+	{
+	private:
+		std::list<GameObject> gameobject_list;
+
+	private:
+		void CreateGameObject();
+		void RemoveGameObject(GameObject);
+	};
+}
