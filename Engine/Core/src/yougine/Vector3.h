453428107d50777739e0f38c86514af576f1a15d
heller77 <koki001230@icloud.com>
2022-08-31 18:16:21 +0900

    namespaceを付けた

--- Engine/Core/src/yougine/Vector3.h
+++ Engine/Core/src/yougine/Vector3.h
@@ -1,26 +1,29 @@
 #pragma once
-struct Vector3
+namespace yougine
 {
-public:
-	float x, y, z;
-
-	Vector3(float x, float y, float z)
-		: x(x),
-		y(y),
-		z(z)
+	struct Vector3
 	{
-	}
+	public:
+		float x, y, z;
 
-	/**
-	 * \brief 代入演算子
-	 * \param v 代入する値あ
-	 * \return 代入された参照
-	 */
-	Vector3& operator=(const Vector3& v)
-	{
-		this->x = v.x;
-		this->y = v.y;
-		this->z = v.z;
-	}
-};
+		Vector3(float x, float y, float z)
+			: x(x),
+			y(y),
+			z(z)
+		{
+		}
 
+		/**
+		 * \brief 代入演算子
+		 * \param v 代入する値あ
+		 * \return 代入された参照
+		 */
+		Vector3& operator=(const Vector3& v)
+		{
+			this->x = v.x;
+			this->y = v.y;
+			this->z = v.z;
+		}
+	};
+
+}
--- Engine/Core/src/yougine/components/TransformComponent.h
+++ Engine/Core/src/yougine/components/TransformComponent.h
@@ -2,14 +2,17 @@
 #include "Component.h"
 #include "./../Vector3.h"
 
-class TransformComponent : public yougine::components::Component
+namespace yougine::components
 {
-private:
-	Vector3* position;
-public:
-	TransformComponent(float x, float y, float z);
-	Vector3 GetPosition();
-	void SetPosition(Vector3 position);
+	class TransformComponent : public yougine::components::Component
+	{
+	private:
+		Vector3* position;
+	public:
+		TransformComponent(float x, float y, float z);
+		Vector3 GetPosition();
+		void SetPosition(Vector3 position);
 
-};
+	};
+}
 
