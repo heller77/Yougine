891f1f6427a33fc27c0161f6882096437e87aa3d
heller77 <koki001230@icloud.com>
2022-09-04 23:47:36 +0900

    整形

--- Engine/Core/src/yougine/managers/RenderManager.h
+++ Engine/Core/src/yougine/managers/RenderManager.h
@@ -6,24 +6,22 @@
 #include "../components/RenderComponent.h"
 namespace yougine::managers
 {
-	class RenderManager
-	{
-	public:
+    class RenderManager
+    {
+    public:
+        void Initialize();
 
-		void Initialize();
+        void RenderScene();
 
-		void RenderScene();
-		GLuint ShaderInit(const std::string& vs_shader_source, const std::string& fs_shader_source);
+        GLuint ShaderInit(const std::string& vs_shader_source, const std::string& fs_shader_source);
 
-	private:
-		void RenderOneGameObject(comoponents::RenderComponent render_component);
+    private:
+        void RenderOneGameObject(comoponents::RenderComponent render_component);
 
+        void MeshBufferInit();
 
+        GLboolean PrintShaderInfoLog(GLuint shader, const char* str);
 
-		void MeshBufferInit();
-
-		GLboolean PrintShaderInfoLog(GLuint shader, const char* str);
-
-		std::string ReadFile(std::string filepath);
-	};
+        std::string ReadFile(std::string filepath);
+    };
 }
