71b2bdfc345aa187c62d2994ddd50d3bd43d1ef2
heller77 <koki001230@icloud.com>
2022-09-19 00:27:51 +0900

    コメントを修正

--- Engine/Core/src/yougine/components/Component.cpp
+++ Engine/Core/src/yougine/components/Component.cpp
@@ -63,7 +63,7 @@ namespace yougine::components
         }
         else if (this->component_name == managers::ComponentName::kNone)
         {
-            //componentname‚ªkNone‚È‚Ì‚ÉComponentList‚É’Ç‰Á‚µ‚æ‚¤‚Æ‚µ‚Ä‚¢‚é‚Ì‚ÅƒGƒ‰[
+            //componentname‚ªkNone‚È‚Ì‚ÅComponentList‚É’Ç‰Á‚µ‚È‚¢
             // throw "throw,this component can not register to ComponentList";
             return false;
         }
