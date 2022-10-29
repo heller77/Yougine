5d241b78800054ed219388d554632b4620a0fabc
heller77 <koki001230@icloud.com>
2022-10-30 00:56:24 +0900

    プロジェクトウィンドーに、表示されるディレクトリのパスを一つ上に移動するボタンと、フォルダとファイルで表示される色を変えた

--- Engine/Core/src/yougine/Editor/ProjectWindow/ProjectWindow.cpp
+++ Engine/Core/src/yougine/Editor/ProjectWindow/ProjectWindow.cpp
@@ -5,16 +5,57 @@
 
 void ProjectWindow::Draw()
 {
-
-    auto a = Projects::Project::GetInstance();
-    std::string path = Projects::Project::GetInstance()->projectFolderPath;
-    //std::string path = "./";
+    auto style = ImGui::GetStyle();
+    // style.ScaleAllSizes(2);
+    //表示するファイルパス
+    std::string path = this->now_display_folderpath;
     ImGui::Begin(editor_windows_manager->GetWindowName(window_name).c_str());
-    auto e = std::filesystem::directory_iterator(path);
-    for(const auto & file:e)
+    auto file_and_folder_list = std::filesystem::directory_iterator(path);
+
+    //表示するパスを上げる
+    if (ImGui::Button("up"))
     {
-        std::string filename = file.path().string();
-        ImGui::Button(filename.c_str()); ImGui::SameLine();
+        auto parent = std::filesystem::path(path).parent_path();
+        std::cout << "parent path : " << parent << std::endl;
+        this->now_display_folderpath = parent.string();
+    }
+    ImGui::SameLine();
+    ImGui::Text(path.c_str());
+    ImGui::Separator();
+
+    ImVec2 button_size(140, 40);
+    //今いる場所のファイルとフォルダを表示
+    for (const auto& file : file_and_folder_list)
+    {
+        auto is_directory = file.is_directory();
+        std::string filename = file.path().filename().string();
+        float hue = 0.1;
+        if (is_directory)
+        {
+            ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(hue, 0.6f, 0.6f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(hue, 0.7f, 0.7f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(hue, 0.8f, 0.8f));
+            if (ImGui::Button(filename.c_str(), button_size))
+            {
+                this->now_display_folderpath += "/" + filename;
+            }
+
+            ImGui::PopStyleColor(3);
+        }
+        else
+        {
+            ImGui::Button(filename.c_str(), button_size);
+        }
+        //ウィンドサイズに合わせて改行したりしなかったり
+        float window_visible_x2 = ImGui::GetWindowPos().x + ImGui::GetWindowContentRegionMax().x;
+        float lastbutton_x = ImGui::GetItemRectMax().x;
+
+        float next_button_x = lastbutton_x + style.ItemSpacing.x + button_size.x;
+
+        if (next_button_x < window_visible_x2)
+        {
+            ImGui::SameLine();
+        }
     }
 
     ImGui::End();
--- Engine/Core/src/yougine/Editor/ProjectWindow/ProjectWindow.h
+++ Engine/Core/src/yougine/Editor/ProjectWindow/ProjectWindow.h
@@ -1,13 +1,17 @@
 ﻿#pragma once
 #include "../EditorWindow.h"
+#include "../../Project/Project.h"
 
 class ProjectWindow : public editor::EditorWindow
 {
+private:
+    std::string now_display_folderpath;
 public:
     void Draw() override;
 
     ProjectWindow(editor::EditorWindowsManager* editor_windows_manager ,yougine::Scene* scene)
         : EditorWindow(editor_windows_manager, editor::EditorWindowName::ProjectWindow)
     {
+        now_display_folderpath = Projects::Project::GetInstance()->projectFolderPath;
     }
 };
