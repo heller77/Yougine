661c9f35636f8058e8dfd4643daee2deb6cc835e
heller77 <koki001230@icloud.com>
2022-12-10 17:26:09 +0900

    ビルド時にDebugフォルダの中身のみコピーするようにした

--- Engine/Core/src/yougine/BuildScript/Builder.cpp
+++ Engine/Core/src/yougine/BuildScript/Builder.cpp
@@ -7,13 +7,11 @@ void builders::Builder::Build(std::string scenfilepath, std::string exportpath)
     auto cmakepath = "";
     std::string cddirectoryCMD = "mkdir tmpbuild & cd tmpbuild & mkdir build & cd build";
     std::string cmakebuildcmd = "cd tmpbuild/build & cmake ./../../../GameBuildProject -G\"Visual Studio 16 2019\" & cmake --build .";
-    std::string copyDebugfolderToExortoathcmd = "xcopy .\\Debug " + exportpath+"\\Debug\\ /s /y";
+    std::string copyDebugfolderToExortoathcmd = "xcopy .\\Debug " + exportpath+" /s /y";
     std::string runcmd = "cd " + exportpath + "& cd Debug &" + " cd & " + "ExeProject.exe";
     std::string cmd = cddirectoryCMD+" & " + cmakebuildcmd + " & " + copyDebugfolderToExortoathcmd + " & " + runcmd;
     std::cout << cmd << std::endl;
     system(cmd.c_str());
-    system("rmdir /S /Q .\\tmpbuild");
-    // system("cd");
-    // system("cd tmpbuild");
-    // system("cd");
+    // system("rmdir /S /Q .\\tmpbuild");
+
 }
