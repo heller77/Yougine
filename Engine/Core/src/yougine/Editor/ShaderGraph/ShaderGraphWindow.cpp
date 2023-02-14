0ff185784678b8146649a6b69a6759858a05f790
uni101 <Hanacchiglon07@gmail.com>
2023-02-15 00:31:52 +0900

    ノード同士のリンクを実装 Disリンクは未実装

--- Engine/Core/src/yougine/Editor/ShaderGraph/ShaderGraphWindow.cpp
+++ Engine/Core/src/yougine/Editor/ShaderGraph/ShaderGraphWindow.cpp
@@ -11,27 +11,96 @@ namespace editor::shadergraph
     {
         ImGui::Begin("simple node editor");
 
+        int nodeID1 = 1;
         ImNodes::BeginNodeEditor();
-        ImNodes::BeginNode(1);
+        ImNodes::BeginNode(nodeID1);
 
         ImNodes::BeginNodeTitleBar();
         ImGui::TextUnformatted("simple node :)");
         ImNodes::EndNodeTitleBar();
 
-        ImNodes::BeginInputAttribute(2);
+        int inputID1 = 2;
+        int outputID1 = 3;
+        ImNodes::BeginInputAttribute(inputID1);
         ImGui::Text("input");
         ImNodes::EndInputAttribute();
 
-        ImNodes::BeginOutputAttribute(3);
+        ImNodes::BeginOutputAttribute(outputID1);
         ImGui::Indent(40);
         ImGui::Text("output");
         ImNodes::EndOutputAttribute();
 
         ImNodes::EndNode();
+
+
+        int nodeID2 = 4;
+        ImNodes::BeginNode(nodeID2);
+
+        ImNodes::BeginNodeTitleBar();
+        ImGui::TextUnformatted("simple node :)");
+        ImNodes::EndNodeTitleBar();
+
+        int inputID2 = 5;
+        int outputID2 = 6;
+        ImNodes::BeginInputAttribute(inputID2);
+        ImGui::Text("input");
+        ImNodes::EndInputAttribute();
+
+        ImNodes::BeginOutputAttribute(outputID2);
+        ImGui::Indent(40);
+        ImGui::Text("output");
+        ImNodes::EndOutputAttribute();
+
+        ImNodes::EndNode();
+
+        PhaseLink();
+
         ImNodes::EndNodeEditor();
+
+        PhaseAddLink();
+        //PhaseDisLink();
 
         ImGui::End();
     }
 
+    void ShaderGraphWindow::PhaseLink()
+    {
+        for (Link& link : links)
+        {
+            ImNodes::Link(link.id, link.start_attr, link.end_attr);
+        }
+    }
 
+    void ShaderGraphWindow::PhaseAddLink()
+    {
+        Link link;
+        if (ImNodes::IsLinkCreated(&link.start_attr, &link.end_attr))
+        {
+            std::cout << "LinkCreated" << std::endl;
+            link.id = ++currentLinks;
+            links.push_back(link);
+        }
+    }
+
+    void ShaderGraphWindow::PhaseDisLink()
+    {
+        int* linkID = 0;
+        ImNodes::GetSelectedLinks(linkID);
+        if (ImNodes::IsLinkSelected(*linkID))
+        {
+            std::cout << "LinkSelected" << std::endl;
+            if (ImNodes::IsLinkDestroyed(linkID))
+            {
+                std::cout << "LinkDestroyed" << std::endl;
+                ImNodes::ClearLinkSelection(*linkID);
+
+                std::vector<Link> newLinks;
+                for (Link ll : links)
+                {
+                    if (ll.id != *linkID) newLinks.push_back(ll);
+                }
+                links = newLinks;
+            }
+        }
+    }
 }
--- Engine/Core/src/yougine/Editor/ShaderGraph/ShaderGraphWindow.h
+++ Engine/Core/src/yougine/Editor/ShaderGraph/ShaderGraphWindow.h
@@ -5,8 +5,25 @@
 
 namespace editor::shadergraph
 {
+    struct Link
+    {
+        int id;
+        int start_attr, end_attr;
+    };
+
     class ShaderGraphWindow : public EditorWindow
     {
+    private:
+        std::vector<Link> links;
+        int currentLinks = 0;
+        int currentSelectedNodeID = 0;
+
+    private:
+        void SearchSelectingNodeID();
+        void PhaseLink();
+        void PhaseAddLink();
+        void PhaseDisLink();
+
     public:
         ShaderGraphWindow(EditorWindowsManager* editor_windows_manager);
         void Draw() override;
