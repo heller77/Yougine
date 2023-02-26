cd2e671519ac88d0beeea7725a7128510c09fdec
heller77 <koki001230@icloud.com>
2023-02-26 23:01:08 +0900

    カメラコンポーネントを作成

---
+++ Engine/Core/src/yougine/components/Camera/CameraComponent.cpp
@@ -0,0 +1,6 @@
+﻿#include "CameraComponent.h"
+
+yougine::components::camera::CameraComponent::CameraComponent() : Component(managers::ComponentName::kNone)
+{
+    CameraComponent::main = std::unique_ptr<CameraComponent>(this);
+}
---
+++ Engine/Core/src/yougine/components/Camera/CameraComponent.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <memory>
+
+#include "../Component.h"
+
+namespace yougine::components::camera
+{
+    class CameraComponent : public yougine::components::Component
+    {
+    private:
+         std::unique_ptr<CameraComponent> main;
+
+    public:
+        CameraComponent();
+    };
+}
