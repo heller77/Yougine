8fcaa1e978af606e84a7d2133ff0ce6245a41603
heller77 <koki001230@icloud.com>
2023-03-01 03:28:52 +0900

    buttonを描画用のクラスのButtonInProjectwindow実装

---
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Button/ButtonInProjectwindow.cpp
@@ -0,0 +1,21 @@
+﻿#include "ButtonInProjectwindow.h"
+
+#include <functional>
+
+#include "imgui/imgui.h"
+
+void editor::projectwindows::Assets::elements::buttons::ButtonInProjectwindow::DrawButton(ImColor default_color, ImColor hover_color, ImColor active_color, std::string display_name, ImVec2 button_size, std::function<void()> clickEvent)
+{
+    float hue = 0.1;
+
+    ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)default_color);
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)hover_color);
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)active_color);
+    if (ImGui::Button(display_name.c_str(), button_size))
+    {
+        clickEvent();
+    }
+
+    ImGui::PopStyleColor(3);
+}
+
