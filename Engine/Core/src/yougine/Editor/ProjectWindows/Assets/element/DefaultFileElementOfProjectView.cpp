ea708ef89d11f676a2b436e0a9f2e848907b74dc
heller77 <koki001230@icloud.com>
2023-03-01 03:41:21 +0900

    プロジェクトビューで特に扱えないファイルを表示するDefaultFileElementOfProjectViewを実装

---
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/DefaultFileElementOfProjectView.cpp
@@ -0,0 +1,29 @@
+﻿#include "DefaultFileElementOfProjectView.h"
+
+#include "Button/ButtonInProjectwindow.h"
+#include "imgui/imgui.h"
+
+void editor::projectwindows::Assets::elements::DefaultFileElementOfProjectView::Select()
+{
+
+}
+
+void editor::projectwindows::Assets::elements::DefaultFileElementOfProjectView::DrawElement()
+{
+    float hue = 0.5;
+
+    auto default_color = ImColor::HSV(hue, 0.6f, 0.6f);
+    auto hover_color = ImColor::HSV(hue, 0.6f, 0.6f);
+    auto buttonactive_color = ImColor::HSV(hue, 0.8f, 0.8f);
+    // std::function<void()> selectfunc = [&]() {return Select(); };
+
+    buttons::ButtonInProjectwindow::DrawButton(default_color, hover_color, buttonactive_color, this->filename, size,
+        [&]() {Select(); });
+}
+
+editor::projectwindows::Assets::elements::DefaultFileElementOfProjectView::DefaultFileElementOfProjectView(
+    std::string filename, ImVec2 size)
+{
+    this->filename = filename;
+    this->size = size;
+}
---
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/DefaultFileElementOfProjectView.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <string>
+
+#include "IElementOfProjectView.h"
+#include "imgui/imgui.h"
+
+namespace editor::projectwindows::Assets::elements {
+    /**
+     * \brief プロジェクトビューで特に扱えないファイル
+     */
+    class DefaultFileElementOfProjectView :public IElementOfProjectView
+    {
+        void Select();
+        std::string filename;
+        ImVec2 size;
+        std::function<void()> selectEvent;
+
+    public:
+        void DrawElement() override;
+
+        DefaultFileElementOfProjectView(std::string filename, ImVec2 size);
+    };
+}
