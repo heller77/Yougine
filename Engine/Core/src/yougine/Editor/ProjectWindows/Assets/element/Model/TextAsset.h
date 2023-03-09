3d0472664c88bf02a77ce3cb372bcfd57efe91f9
heller77 <koki001230@icloud.com>
2023-03-09 23:21:14 +0900

    不要なinclude削除

--- Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/TextAsset.h
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/TextAsset.h
@@ -1,7 +1,5 @@
 ﻿#pragma once
 #include "Asset.h"
-#include "../Presenter/ElementOfProjectWindowPresenter.h"
-
 namespace editor::projectwindows::assets::elements::model
 {
     class TextAsset :public Asset
