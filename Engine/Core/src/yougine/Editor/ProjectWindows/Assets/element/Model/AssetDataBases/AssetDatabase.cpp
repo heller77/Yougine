25c26e9b1051d793a348a27b24835b551379059f
heller77 <koki001230@icloud.com>
2023-03-14 22:35:48 +0900

    アセットをidをキーとしてunordered_mapで管理するAssetDatabaseを作成

---
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/AssetDataBases/AssetDatabase.cpp
@@ -0,0 +1,25 @@
+﻿#include "AssetDatabase.h"
+
+#include <iostream>
+
+editor::projectwindows::assets::elements::model::assetdatabases::AssetDatabase::AssetDatabase()
+{
+
+}
+
+void editor::projectwindows::assets::elements::model::assetdatabases::AssetDatabase::AddAsset(
+    std::shared_ptr<utility::youginuuid::YougineUuid> uuid, std::shared_ptr<Asset> asset)
+{
+    if (asset == nullptr)
+    {
+        std::cerr << "asset is null";
+        return;
+    }
+    this->asset_map[uuid->convertstring()] = asset;
+}
+
+// void editor::projectwindows::assets::elements::model::assetdatabases::AssetDatabase::AddAsset(int asset_id,
+//                                                                                               std::shared_ptr<Asset> asset)
+// {
+//     this->asset_map[asset_id] = asset;
+// }
