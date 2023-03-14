24f041389e5759137b8f79b7e223b00c652bd5c0
heller77 <koki001230@icloud.com>
2023-03-14 18:32:02 +0900

    YougineUuidクラスを作成

---
+++ Engine/Core/src/yougine/utilitys/uuid/YougineUuid.cpp
@@ -0,0 +1,17 @@
+﻿#include "YougineUuid.h"
+
+utility::youginuuid::YougineUuid::YougineUuid()
+{
+    std::random_device rd;
+    auto seed_data = std::array<int, std::mt19937::state_size> {};
+    std::generate(std::begin(seed_data), std::end(seed_data), std::ref(rd));
+    std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+    std::mt19937 generator(seq);
+    uuids::uuid_random_generator gen{ generator };
+    this->uuid = gen();
+}
+
+std::string utility::youginuuid::YougineUuid::convertstring()const
+{
+    return to_string(this->uuid);
+}
