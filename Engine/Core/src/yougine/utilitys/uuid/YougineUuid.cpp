d71ffdfbbdccc5bd2fd25d8257d5e5205c05bf89
heller77 <koki001230@icloud.com>
2023-03-16 23:11:21 +0900

    youginuuidでuuidを生成するときにコピーするようにした
    以前のままだとuuidを生成したコンストラクターを出したら死んでしまうので、newでつくらないと変な感じになる(なんかコンストラクターの後でゲットすると値が変わってる)

--- Engine/Core/src/yougine/utilitys/uuid/YougineUuid.cpp
+++ Engine/Core/src/yougine/utilitys/uuid/YougineUuid.cpp
@@ -8,18 +8,29 @@ utility::youginuuid::YougineUuid::YougineUuid()
     std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
     std::mt19937 generator(seq);
     uuids::uuid_random_generator gen{ generator };
-    auto tmp_uuid = gen();
-    this->uuid = std::shared_ptr<uuids::uuid>(&tmp_uuid, [](uuids::uuid*) {});
+    auto id = gen();
+
+    auto uuid = new uuids::uuid(id);//これでコピーできてる？
+    this->uuid = std::shared_ptr<uuids::uuid>(uuid);
 
 }
 
 utility::youginuuid::YougineUuid::YougineUuid(std::string uuid_string)
 {
     auto id = uuids::uuid::from_string(uuid_string).value();
-    this->uuid = std::shared_ptr<uuids::uuid>(&id, [](uuids::uuid*) {});
+    auto uuid = new uuids::uuid(id);
+    this->uuid = std::shared_ptr<uuids::uuid>(uuid);
+    // this->uuid = std::shared_ptr<uuids::uuid>(&id, [](uuids::uuid*) {});
+    // this->uuid_text = uuid_string;
 }
 
 std::string utility::youginuuid::YougineUuid::convertstring()const
 {
-    return to_string(*this->uuid.get());
+    return to_string(*this->uuid);
+    // return this->uuid_text;
 }
+
+// std::string utility::youginuuid::YougineUuid::GetUUidfromptr()
+// {
+//     return to_string(*this->uuid);
+// }
--- Engine/Core/src/yougine/utilitys/uuid/YougineUuid.h
+++ Engine/Core/src/yougine/utilitys/uuid/YougineUuid.h
@@ -7,10 +7,14 @@ namespace utility::youginuuid
     {
     private:
         std::shared_ptr<uuids::uuid> uuid;
+        // uuids::uuid* uuid_default_ptr;
+        // std::string uuid_text;
     public:
         YougineUuid();
         YougineUuid(std::string uuid_string);
         std::string convertstring()const;
+        // std::string GetUUidfromptr();
+
 
 
         bool operator==(const YougineUuid& rhs)const
@@ -22,9 +26,9 @@ namespace utility::youginuuid
     struct Hash
     {
     public:
-        inline size_t operator()(const std::string uuid_string)const
+        inline size_t operator()(const YougineUuid uuid_string)const
         {
-            return std::hash<std::string>()(uuid_string);
+            return std::hash<std::string>()(uuid_string.convertstring());
         }
 
     };
