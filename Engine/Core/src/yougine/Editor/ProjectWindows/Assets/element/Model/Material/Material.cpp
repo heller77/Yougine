7413b45cdc8f63bcd415c60b4e6160b6dcd6a028
heller77 <koki001230@icloud.com>
2023-03-19 18:39:03 +0900

    パラメータ設定をInitialize関数でまとめた

--- Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Material/Material.cpp
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Material/Material.cpp
@@ -4,22 +4,25 @@
 
 #include "../AssetInfoExporter/AssetInfoFileExporter.h"
 
+void editor::projectwindows::assets::elements::model::materials::Material::Initialize()
+{
+    auto assetoption = std::make_shared<inspectorwindows::assetviews::options::AssetViewOption>(false, true);
+    this->parameter["path"] = std::make_shared<assetparameters::Parameter>(this->path.string(), assetoption);
+}
+
 editor::projectwindows::assets::elements::model::materials::Material::Material(const std::filesystem::path& path,
     const std::shared_ptr<utility::youginuuid::YougineUuid>& uuid) :Asset(path, uuid)
 {
     // this->frag_asset_uuid = utility::youginuuid::YougineUuid::GenerateNullUuid();
     // this->vert_asset_uuid = utility::youginuuid::YougineUuid::GenerateNullUuid();
-    auto assetoption = std::make_shared<inspectorwindows::assetviews::options::AssetViewOption>(false, true);
-    this->parameter["path"] = std::make_shared<assetparameters::Parameter>(this->path.string(), assetoption);
+    Initialize();
 }
-
 editor::projectwindows::assets::elements::model::materials::Material::Material(
     const std::filesystem::path& assetinfo_file_path) :Asset(assetinfo_file_path)
 {
     // this->frag_asset_uuid = utility::youginuuid::YougineUuid::GenerateNullUuid();
     // this->vert_asset_uuid = utility::youginuuid::YougineUuid::GenerateNullUuid();
-    auto assetoption = std::make_shared<inspectorwindows::assetviews::options::AssetViewOption>(false, true);
-    this->parameter["path"] = std::make_shared<assetparameters::Parameter>(this->path.string(), assetoption);
+    Initialize();
 }
 
 void editor::projectwindows::assets::elements::model::materials::Material::Export()
