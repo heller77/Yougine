4fbc3bd2e63705f693fd80477c1eae8b79f05911
heller77 <koki001230@icloud.com>
2023-03-19 18:51:10 +0900

    パスの初期化をミスっていたので修正

--- Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Asset.cpp
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Asset.cpp
@@ -14,7 +14,7 @@ editor::projectwindows::assets::elements::model::Asset::Asset(std::filesystem::p
 editor::projectwindows::assets::elements::model::Asset::Asset(const std::filesystem::path assetinfo_file_path)
 {
 
-    this->path = assetinfo_file_path.stem().string();
+    this->path = assetinfo_file_path.parent_path().string() + "\\" + assetinfo_file_path.stem().string();
     using json = nlohmann::ordered_json;
 
     std::ifstream reading(assetinfo_file_path.string(), std::ios::in);
