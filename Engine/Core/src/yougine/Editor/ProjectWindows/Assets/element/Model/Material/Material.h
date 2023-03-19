6fe3b68766d9a63c9bc3219211a5d83db2b9f7af
heller77 <koki001230@icloud.com>
2023-03-19 16:23:43 +0900

    マテリアルクラス作成

---
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Material/Material.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "../../../../../../utilitys/uuid/YougineUuid.h"
+#include "../shader/ShaderFileAsset.h"
+
+namespace editor::projectwindows::assets::elements::model::materials
+{
+    class Material :public  Asset
+    {
+    private:
+        shader::ShaderFileAsset vert_asset_uuid;
+        shader::ShaderFileAsset frag_asset_uuid;
+    public:
+
+    };
+}
