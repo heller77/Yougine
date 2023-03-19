4a640ea1fbad94133a817072c4d6bb4990a58979
heller77 <koki001230@icloud.com>
2023-03-19 18:51:57 +0900

    .h入れるの忘れてた（Initializei入れた時に）

--- Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Material/Material.h
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Material/Material.h
@@ -8,6 +8,7 @@ namespace editor::projectwindows::assets::elements::model::materials
     private:
         std::shared_ptr<shader::ShaderFileAsset> vert_asset_uuid;
         std::shared_ptr < shader::ShaderFileAsset> frag_asset_uuid;
+        void Initialize();
     public:
         Material(const std::filesystem::path& path, const std::shared_ptr<utility::youginuuid::YougineUuid>& uuid);
 
