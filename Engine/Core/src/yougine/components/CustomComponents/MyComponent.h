73014f2c51a0d506b8c3dfb97c03fb0e8257b81f
heller77 <koki001230@icloud.com>
2023-09-10 21:51:59 +0900

    カスタムコンポーネントの一種としてMyComponentを定義

---
+++ Engine/Core/src/yougine/components/CustomComponents/MyComponent.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "CutomComponent.h"
+
+namespace yougine::components::customcomponents
+{
+    class MyComponent : public CutomComponent
+    {
+    public:
+        void Update() override;
+    };
+}
