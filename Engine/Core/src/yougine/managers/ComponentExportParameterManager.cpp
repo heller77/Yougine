2c43b50cd43106f08a1c4726f0c1c793c6ee2592
heller77 <koki001230@icloud.com>
2024-03-10 00:44:12 +0900

    コンポーネントからエディターにエクスポートするパラメータを管理するクラスを作成

---
+++ Engine/Core/src/yougine/managers/ComponentExportParameterManager.cpp
@@ -0,0 +1,14 @@
+﻿#include "ComponentExportParameterManager.h"
+
+void yougine::managers::ComponentExportParameterManager::Add(std::shared_ptr<utility::youginuuid::YougineUuid> uuid,
+    std::shared_ptr<ComponentExportParameter> serializefield)
+{
+    this->serializefield_list[uuid] = serializefield;
+}
+
+std::shared_ptr<yougine::managers::ComponentExportParameter> yougine::managers::ComponentExportParameterManager::
+GetSerializeField(std::shared_ptr<utility::youginuuid::YougineUuid> uuid)
+{
+    return this->serializefield_list[uuid];
+}
+
---
+++ Engine/Core/src/yougine/managers/ComponentExportParameterManager.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <any>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "./../utilitys/uuid/YougineUuid.h"
+
+namespace yougine::managers
+{
+    class ComponentExportParameter
+    {
+    private:
+        std::vector<std::vector<std::any>> accessable_properties_list;
+    };
+
+    /*
+     * シーンにあるcomponent全てのSerializefieldのリストを使う
+     */
+    class ComponentExportParameterManager
+    {
+    private:
+        std::map<std::shared_ptr<utility::youginuuid::YougineUuid>, std::shared_ptr<ComponentExportParameter>> serializefield_list;
+    public:
+        void Add(std::shared_ptr<utility::youginuuid::YougineUuid> uuid, std::shared_ptr<ComponentExportParameter> serializefield);
+        std::shared_ptr<ComponentExportParameter> GetSerializeField(std::shared_ptr<utility::youginuuid::YougineUuid> uuid);
+
+    };
+}
