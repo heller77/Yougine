e60f151af304c8cb179462f10c622be70bfb194c
heller77 <koki001230@icloud.com>
2024-03-17 02:13:09 +0900

    ComponentExportParameterにサイズと値が取れる関数を実装

--- Engine/Core/src/yougine/components/ComponentExportParameters/ComponentExportParameter.cpp
+++ Engine/Core/src/yougine/components/ComponentExportParameters/ComponentExportParameter.cpp
@@ -1,1 +1,11 @@
 ﻿#include "ComponentExportParameter.h"
+
+int yougine::components::componentexportparameter::ComponentExportParameter::GetSize() const
+{
+    return this->accessable_properties_list.size();
+}
+
+std::any yougine::components::componentexportparameter::ComponentExportParameter::Get(int index)
+{
+    return this->accessable_properties_list[index];
+}
--- Engine/Core/src/yougine/components/ComponentExportParameters/ComponentExportParameter.h
+++ Engine/Core/src/yougine/components/ComponentExportParameters/ComponentExportParameter.h
@@ -3,9 +3,15 @@
 #include <any>
 namespace yougine::components::componentexportparameter
 {
+    /**
+     * \brief コンポーネントがエクスポートするパラメータを管理
+     */
     class ComponentExportParameter
     {
     private:
         std::vector<std::vector<std::any>> accessable_properties_list;
+    public:
+        int GetSize() const;
+        std::any Get(int index);
     };
 }
