d06e94bb49de39dc955e3545794997b84c7ea351
heller77 <koki001230@icloud.com>
2024-03-20 02:12:56 +0900

    コメント追加

--- Engine/UserEngineCommon/include/UserShare/managers/UserScriptComponentEntryPointManager.h
+++ Engine/UserEngineCommon/include/UserShare/managers/UserScriptComponentEntryPointManager.h
@@ -9,7 +9,15 @@ namespace yougine::managers
     public:
         UserScriptComponentEntryPointManager();
 
+        /**
+         * \brief update関数を追加する
+         * \param method
+         */
         void AddUpdate(std::function<void(void)> method);
+
+        /**
+         * \brief updateを実行する（ユーザは呼ばないで！）
+         */
         void Update();
 
     };
