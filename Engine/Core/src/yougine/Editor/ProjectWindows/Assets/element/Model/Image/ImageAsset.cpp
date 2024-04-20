2c7efcac8f0f145aff12e2bba2ba32525e930aec
heller77 <koki001230@icloud.com>
2024-04-20 18:05:25 +0900

    イメージの初期化時にエラーがでるように

--- Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Image/ImageAsset.cpp
+++ Engine/Core/src/yougine/Editor/ProjectWindows/Assets/element/Model/Image/ImageAsset.cpp
@@ -27,31 +27,42 @@ void editor::projectwindows::assets::elements::model::image::ImageAsset::Initial
 
 void editor::projectwindows::assets::elements::model::image::ImageAsset::Initialize()
 {
+
     auto assetfile = this->GetFilePath();
+
+    std::cout << "texture  : " << assetfile.string() << std::endl;
+
     int width, height, channels;
     image = stbi_load(assetfile.string().c_str(), &width, &height, &channels, 0);
     if (image == nullptr) {
         std::cerr << "Error: Image could not be loaded." << std::endl;
     }
+    yougine::managers::RenderManager::geterror("pre image glgentexture(so context is not good ?)");
     textureID = -1;
     // テクスチャオブジェクトの生成
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
+    std::cout << "texture id : " << textureID << std::endl;
+    yougine::managers::RenderManager::geterror("image asset initialize[glGenTextures glBindTexture] , textureid : ");
+
 
     // テクスチャパラメータの設定
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    yougine::managers::RenderManager::geterror("image asset initialize[glTexParameteri ]");
+    std::cout << "texparameter -- " << std::endl;
 
     // テクスチャへ画像データをgpuにアップロード
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, channels == 4 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, image);
+    yougine::managers::RenderManager::geterror("image asset initialize[glTexImage2D ]");
 
     // cpu側のメモリを解放
     stbi_image_free(image);
 
     glBindTexture(GL_TEXTURE_2D, 0);
-    yougine::managers::RenderManager::geterror("image asset initialize");
+    yougine::managers::RenderManager::geterror("image asset initialize glBindTexture");
 }
 
 editor::projectwindows::assets::elements::model::image::ImageAsset::ImageAsset(std::filesystem::path path,
